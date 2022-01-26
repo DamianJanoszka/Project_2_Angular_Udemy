de3a676b1b2709203bfc3c20a226ed4b158eed0b
d.janoszka <d.janoszka@wasko.pl>
2022-01-26 15:29:14 +0100

    Added forms to the app / Learning about pipes / Learning about Http requests

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { Router,ActivatedRoute } from '@angular/router';
 import {Recipe} from '../recipe.model';
+import{ Subscription } from 'rxjs/Subscription';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(private recipeService: RecipeService,
     private router: Router,
@@ -16,10 +19,19 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
   this.recipes = this.recipeService.getRecipes();
+  this.subscription = this.recipeService.recipesChanged
+    .subscribe(
+    (recipes: Recipe[]) => {
+      this.recipes=recipes;
+    });
   }
 
   onNewRecipe(){
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -32,7 +32,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       }
      );
   }
-  onAddItem(form: NgForm){
+  onSubmit(form: NgForm){
    const value = form.value;
     const newIngredient = new Ingredient(value.name,value.amount);
     if (this.editMode) {
@@ -40,6 +40,19 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.shoppingListService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    // cleaning input data from form after submit
+    form.reset();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+  this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
   }
 
   ngOnDestroy() {
